73e80ffce9a3d13e1c4cd629dbe7e06e95a6f256
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-08-18 15:29:18 +0300

    Added XUnitTest

---
+++ UnitTest.APP/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTest.APP
+{
+   public class Calculator
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+        public int Extract(int a,int b)
+        {
+            if (a > b)
+                return a - b;
+            else if (b > a)
+                return b - a;
+            else
+                return 0;
+            
+        }
+    }
+}
---
+++ XUnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using XUnitTest.APP;
+
+namespace XUnitTest.Test
+{
+   public class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            //Arrange
+            int a = 5;
+            int b = 20;
+            var calculator =new Calculator();
+            //Act 
+            var total = calculator.Add(a, b);
+            //Assert
+            Assert.Equal<int>(25,total);
+        }
+        [Fact]
+        public void ExtractTest()
+        {
+            ////Arrange
+            //int a = 5;
+            //int b = 5;
+            //var calculator = new Calculator();
+            ////Act
+            //var extraction = calculator.Extract(a, b);
+            ////Assert
+            //Assert.Equal<int>(1, extraction);
+            Assert.DoesNotContain("Furkan", "Furkan Fidan");
+        }
+    }
+}
